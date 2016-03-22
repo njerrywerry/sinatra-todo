9c9ed2a225e45a5d0c9fdc312b46f56de91f2f4b
Sylvia <sylviakieha@gmail.com>
2016-03-22 23:45:28 +0300

    editing a note

--- recall.rb
+++ recall.rb
@@ -30,3 +30,18 @@ post '/' do
   n.save
   redirect '/'
 end
+
+get '/:id' do
+  @note = Note.get params[:id]
+  @title = "Edit note ##{params[:id]}"
+  erb :edit
+end
+
+put '/:id' do
+  n = Note.get params[:id]
+  n.content = params[:content]
+  n.complete = params[:complete] ? 1 : 0
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
