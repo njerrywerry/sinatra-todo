cd23046d9309cf86812239efaeff72eb67f10bde
Sylvia <sylviakieha@gmail.com>
2016-03-24 01:34:21 +0300

    fixing flash messages throughout the app

--- recall.rb
+++ recall.rb
@@ -5,6 +5,8 @@ require 'tilt/builder'
 require 'sinatra/flash'
 require 'sinatra/redirect_with_flash'
 
+enable :sessions
+
 SITE_TITLE = "Recall"
 SITE_DESCRIPTION = "'cause you're too busy to remember"
 
@@ -38,7 +40,7 @@ get '/' do
   @notes = Note.all :order => :id.desc
   @title = 'All Notes'
   if @notes.empty?
-    flash.sweep[:error] = 'No notes found. Add first note below.'
+    flash[:error] = 'No notes found. Add first note below.'
   end
   erb :home
 end
@@ -48,41 +50,67 @@ post '/' do
   n.content = params[:content]
   n.created_at = Time.now
   n.updated_at = Time.now
-  n.save
-  redirect '/'
+  if n.save
+    redirect '/', notice: 'Note created successfully.'
+  else
+    redirect '/', error: 'Failed to save note.'
+  end
 end
 
 get '/:id' do
   @note = Note.get params[:id]
   @title = "Edit note ##{params[:id]}"
-  erb :edit
+  if @note
+    erb :edit
+  else
+    redirect '/', error: "Can't find that note."
+  end
 end
 
 put '/:id' do
   n = Note.get params[:id]
+  unless n
+    redirect '/', error: "Can't find that note."
+  end
   n.content = params[:content]
   n.complete = params[:complete] ? 1 : 0
   n.updated_at = Time.now
-  n.save
-  redirect '/'
+  if n.save
+    redirect '/', notice: 'Note updated successfully.'
+  else
+    redirect '/', error: 'Error updating note.'
+  end
 end
 
 get '/:id/delete' do
   @note = Note.get params[:id]
   @title = "Confirm deletion of note ##{params[:id]}"
-  erb :delete
+  if @note
+    erb :delete
+  else
+    redirect '/', error: "Can't find that note."
+  end
 end
 
 delete '/:id' do
   n = Note.get params[:id]
-  n.destroy
-  redirect '/'
+  if n.destroy
+    redirect '/', notice: 'Note deleted successfully.'
+  else
+    redirect '/', error: 'Error deleting note.'
+  end
 end
 
 get '/:id/complete' do
   n = Note.get params[:id]
+  unless n
+    redirect '/', error: "Can't find that note."
+  end
   n.complete = n.complete ? 0 : 1
   n.updated_at = Time.now
-  n.save
-  redirect '/'
+  if n.save
+    redirect '/', notice: 'Note marked as complete.'
+  else
+    redirect '/', error: 'Error marking note as complete.'
+  end
 end
